2f042c5b52678987ab239bd2e1a478203e4f852c
vbrazas <appleitunesstore11@gmail.com>
2018-12-29 18:32:55 +0200

    synchronous checker done

--- filler_checker.zsh
+++ filler_checker.zsh
@@ -1,114 +1,66 @@
-#!/bin/bash
+#!/bin/zsh
 
-# That file needs to be launched in filler/resources folder
-# Usage: ./test.zsh
+# That checker needs to be launched in filler/resources folder
+# Usage: ./filler_checker.zsh
 
-TEST_D="testing/"
-MY_FILLER="./azavrazh.filler"
-PLAYERS_D="players/"; PLAYERS="abanlin carli champely grati hcao superjeannot"
-MAPS_D="maps/"; MAPS="map00 map01 map02"
+#### Colors definitions
+GREEN='\e[0;32m'
+RED='\e[0;31m'
+YELLOW='\e[93m'
+CYAN='\e[96m'
+MAG='\e[95m'
+BLUE='\e[1;34m'
+RESET='\e[0m'
+
+fill_arg()
+{
+	if [[ ! $2 ]]; then
+		echo -n "Input path to the $3 path: "; read -r tmp
+		ls $tmp &> /dev/null
+		if (( $? )); then
+			echo "${RED}Error: wrong path: $tmp"
+			echo "${BLUE}Usage: $1 [xlogin.filler name] [filler_vm path]"
+			exit 1;
+		fi
+	else
+		tmp=$2
+	fi
+	eval "$4=$tmp"
+}
+fill_arg $0 "$1" "xlogin.filler" FILE_TO_CHECK
+fill_arg $0 "$2" "./filler_vm" VM_PATH
+
+PLAYERS_D="$VM_PATH/players/"; PLAYERS="abanlin carli champely grati hcao superjeannot"
+MAPS_D="$VM_PATH/maps/"; MAPS="map00 map01 map02"
 NUM_TESTS=5
 
-
-print_results()
-{
-GREEN='\033[0;32m'
-RED='\033[0;31m'
-RESET='\033[0m'
-
-FILES1=$(ls -1 $TEST_D | grep -E ".*_.*_p1")
-FILES2=$(ls -1 $TEST_D | grep -E ".*_.*_p2")
-cd $TEST_D
-
-put_all()
-{
-	res_me=($1)
-	res_evil=($2)
-
-	me_strs=${#res_me[@]}
-	evil_strs=${#res_evil[@]}
-	if [[ $me_strs != $evil_strs ]];
-	then
-		echo $me_strs
-		echo $evil_strs
-		echo "ACHTUNG!!!"
-		return
-	fi
-
-	me_count=0
-	for (( i=0; i<$me_strs; i++ ));
-	do
-		if [[ ${res_me[i]} -gt ${res_evil[i]} ]];
-		then
-			let me_count++;
-		fi
-	done
-	if [[ $me_count -gt $(($me_strs/2)) ]];
-	then
-		printf $GREEN
-	else
-		printf $RED
-	fi
-	echo "In $fname I win $me_count/$me_strs"
-}
-
-	for	fname in $FILES1;
-	do
-		res_me=$(cat $fname | grep "== O" | cut -d: -f2)
-		res_evil=$(cat $fname | grep "== X" | cut -d: -f2)
-		put_all "$res_me" "$res_evil"
-	done
-
-	for	fname in $FILES2;
-	do
-		res_me=$(cat $fname | grep "== X" | cut -d: -f2)
-		res_evil=$(cat $fname | grep "== O" | cut -d: -f2)
-		put_all "$res_me" "$res_evil"
-	done
-
-cd ..
-printf $RESET
-}
-
-wait_for_all()
-{
-	sleep_val=10
-	while [[ $(pgrep ruby | wc -l) -gt 0 ]];
-	do
-		echo -n "Waiting... for "
-		echo -n $(pgrep ruby | wc -l)
-		echo " proccesses"
-		sleep $sleep_val
-	done
-}
+(( formula_num = ($(echo $MAPS | wc -w) * $(echo $PLAYERS | wc -w) * $NUM_TESTS) / 3 ))
 sychronize()
 {
-	magic_num="1"
-	proc_num=$(pgrep ruby | wc -l)
-	formula_num=$(echo $(echo $MAPS | wc -w) \* $NUM_TESTS \* 2 \* $magic_num - 1 | bc | cut -d. -f1)
-	if [[ $proc_num -gt $formula_num ]];
-	then
-		wait_for_all
-	fi
+	for (( i=$(pgrep ruby | wc -l); i>$1; i=$(pgrep ruby | wc -l) )); do
+		echo "Waiting... for $i proccesses"
+		sleep 10
+	done
 }
 
-rm -rf $TEST_D
-mkdir -p $TEST_D
-for	pname in $PLAYERS;
-do
-	for (( i=0; i<$NUM_TESTS; i++ ));
-	do
-		for mapname in $MAPS;
-		do
-			echo -n > $TEST_D/${pname}_${mapname}_p1
-			echo -n > $TEST_D/${pname}_${mapname}_p2
-			echo "Testing -p1 $MY_FILLER -p2 players/${pname}.filler ON $mapname"
-			./filler_vm -f ${MAPS_D}$mapname -p1 $MY_FILLER -p2 ${PLAYERS_D}${pname}.filler | grep "==" >> ${TEST_D}${pname}_${mapname}_p1 &
-			echo "Testing -p2 $MY_FILLER -p1 players/${pname}.filler ON $mapname"
-			./filler_vm -f ${MAPS_D}$mapname -p2 $MY_FILLER -p1 ${PLAYERS_D}${pname}.filler | grep "==" >> ${TEST_D}${pname}_${mapname}_p2 &
-			sychronize
+check_winner()
+{
+	if [[ $1 -eq "O" ]]; then
+		if (( $2 >= $3 )); then color=$GREEN; else color=$RED; fi
+	elif [[ $1 -eq "X" ]]; then
+		if (( $3 >= $2 )); then color=$GREEN; else color=$RED; fi
+	fi
+	echo $4
+	echo $color "$2 VS $3" $RESET
+}
+
+for mapname in `echo $MAPS`; do
+	for (( i = 0; i < $NUM_TESTS; i++ )); do
+		for	pname in `echo $PLAYERS`; do
+			check_winner O $($VM_PATH/filler_vm -q -f ${MAPS_D}$mapname -p1 ./$FILE_TO_CHECK -p2 ${PLAYERS_D}${pname}.filler | grep "==" | awk '{printf $4 " "}') "$FILE_TO_CHECK VS ${pname}.filler ON $mapname" &
+			check_winner X $($VM_PATH/filler_vm -q -f ${MAPS_D}$mapname -p2 ./$FILE_TO_CHECK -p1 ${PLAYERS_D}${pname}.filler | grep "==" | awk '{printf $4 " "}') "${pname}.filler VS $FILE_TO_CHECK ON $mapname" &
+			# sychronize $formula_num
 		done
 	done
 done
-wait_for_all
-print_results
+sychronize 0
