409846f0618457649877b0ce30c4d5a5a293f746
vbrazas <appleitunesstore11@gmail.com>
2018-12-29 20:18:28 +0200

    untested final version

--- filler_checker.zsh
+++ filler_checker.zsh
@@ -30,37 +30,53 @@ fill_arg()
 fill_arg $0 "$1" "xlogin.filler" FILE_TO_CHECK
 fill_arg $0 "$2" "./filler_vm" VM_PATH
 
+TEMPFILE=".tmpfile"
 PLAYERS_D="$VM_PATH/players/"; PLAYERS="abanlin carli champely grati hcao superjeannot"
 MAPS_D="$VM_PATH/maps/"; MAPS="map00 map01 map02"
 NUM_TESTS=5
 
-(( formula_num = ($(echo $MAPS | wc -w) * $(echo $PLAYERS | wc -w) * $NUM_TESTS) / 3 ))
+(( formula_num = ($(echo $MAPS | wc -w) * $(echo $PLAYERS | wc -w) * $NUM_TESTS) / 5 ))
 sychronize()
 {
 	for (( i=$(pgrep ruby | wc -l); i>$1; i=$(pgrep ruby | wc -l) )); do
-		echo "Waiting... for $i proccesses"
+		echo "${YELLOW}Waiting... for $i proccesses"
 		sleep 10
 	done
 }
 
-check_winner()
-{
-	if [[ $1 -eq "O" ]]; then
-		if (( $2 >= $3 )); then color=$GREEN; else color=$RED; fi
-	elif [[ $1 -eq "X" ]]; then
-		if (( $3 >= $2 )); then color=$GREEN; else color=$RED; fi
-	fi
-	echo $4
-	echo $color "$2 VS $3" $RESET
-}
-
+echo > $TEMPFILE
 for mapname in `echo $MAPS`; do
 	for (( i = 0; i < $NUM_TESTS; i++ )); do
 		for	pname in `echo $PLAYERS`; do
-			check_winner O $($VM_PATH/filler_vm -q -f ${MAPS_D}$mapname -p1 ./$FILE_TO_CHECK -p2 ${PLAYERS_D}${pname}.filler | grep "==" | awk '{printf $4 " "}') "$FILE_TO_CHECK VS ${pname}.filler ON $mapname" &
-			check_winner X $($VM_PATH/filler_vm -q -f ${MAPS_D}$mapname -p2 ./$FILE_TO_CHECK -p1 ${PLAYERS_D}${pname}.filler | grep "==" | awk '{printf $4 " "}') "${pname}.filler VS $FILE_TO_CHECK ON $mapname" &
-			# sychronize $formula_num
+			$VM_PATH/filler_vm -q -f ${MAPS_D}$mapname -p1 ./$FILE_TO_CHECK -p2 ${PLAYERS_D}${pname}.filler | grep -B 2 "==" >> $TEMPFILE &
+			$VM_PATH/filler_vm -q -f ${MAPS_D}$mapname -p2 ./$FILE_TO_CHECK -p1 ${PLAYERS_D}${pname}.filler | grep -B 2 "==" >> $TEMPFILE &
+			sychronize $formula_num
 		done
 	done
 done
 sychronize 0
+
+i=1
+while read -r line; do
+	if (( i==1 )); then
+		let p1=$(echo $line | awk '{print $2}')
+	elif (( i==2 )); then
+		let p2=$(echo $line | awk '{print $2}')
+	elif (( i==3 )); then
+		let score1=$(echo $line | awk '{print $4}')
+	elif (( i==4 )); then
+		let score2=$(echo $line | awk '{print $4}')
+		i=0
+	fi
+
+	if (( i==0 )); then
+		if [[ $(echo $p1 | grep "$FILE_TO_CHECK") ]]; then
+			if (( $score1 >= $score2 )); then color=$GREEN; else color=$RED; fi
+		elif [[ $(echo $p2 | grep "$FILE_TO_CHECK") ]]; then
+			if (( $score2 >= $score1 )); then color=$GREEN; else color=$RED; fi
+		fi
+		echo "${color}$p1 VS $p2 => $score1 VS $score2${RESET}"
+	fi
+	let i++
+
+done < "$TEMPFILE"
