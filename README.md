cba0132b95dbd7665df0e52a11454c74cfd88cf0
vbrazas <appleitunesstore11@gmail.com>
2019-01-26 04:18:59 +0200

    finalize

--- README.md
+++ README.md
@@ -3,8 +3,9 @@
 
 ## ACHTUNG
 
-./fillit_checker parse output of the `./filler_vm -q` command<br />
+./filler_checker parse output of the `./filler_vm -q` command<br />
 Example of this output:
+
 ```
 # -------------- VM  version 1.1 ------------- #
 #                                              #
@@ -16,14 +17,33 @@ launched ./vbrazas/vbrazas.filler
 == O fin: 0
 == X fin: 169
 ```
-You can always get more information about checker if you take look on its source code in *fillit_checker.zsh* file<br />
+
+You can always get more information about checker if you take look on its source code in *filler_checker.zsh* file
 
 ## How to install ?
 
-	git clone https://github.com/millon15/fillit_checker
+	git clone https://github.com/millon15/filler_checker
 
 ## How to use ?
 
 	chmod +x filler_checker.zsh
 
-	Usage: ./filler_checker.zsh [xlogin.filler name] [filler_vm path]
+```bash
+Usage: ./filler_checker.zsh [xlogin.filler] [filler_vm_path]
+Example: ./filler_checker.zsh ../filler/vbrazas.filler resources/
+```
+
+#### NOTE: List of maps and players to test you can always change in the head of the ./filler_checker.zsh files
+
+```bash
+PLAYERS="abanlin carli champely grati hcao superjeannot"    #### <<<<<<<< change it!
+---------------------------------------
+PLAYERS="abanlin carli champely"                            #### <<<<<<<< on it, for example.
+```
+
+```bash
+MAPS="map00 map01"          #### <<<<<<<< or it!
+# map02"
+---------------------------------------
+MAPS="map00 map01 map02"    #### <<<<<<<< on it!
+```
--- filler_checker.zsh
+++ filler_checker.zsh
@@ -1,15 +1,5 @@
 #!/bin/zsh
 
-PLAYERS="abanlin"
-# carli champely grati hcao superjeannot"
-MAPS="map00"
-# map01 map02"
-
-MAX_NUM_OF_FILLER_VM_PRCS=20
-NUM_TESTS=5
-TEMPFILE=".tmpfile"
-
-#### Colors definitions
 GREEN='\e[0;32m'
 RED='\e[0;31m'
 YELLOW='\e[93m'
@@ -18,12 +8,24 @@ MAG='\e[95m'
 BLUE='\e[1;34m'
 RESET='\e[0m'
 
+PLAYERS="abanlin carli champely"
+# grati hcao superjeannot"
+MAPS="map00 map01"
+# map02"
+
+NUM_TESTS=5
+TEMPFILE=".tmpfile"
+MAX_NUM_OF_FILLER_VM_PROCESSES=12
+
+
 check_arg()
 {
 	eval "$1 &> /dev/null"
 	if (( $? != $2 )); then
-		echo "${RED}Error: wrong path: $tmp"
-		echo "${BLUE}Usage: $3 [xlogin.filler name] [filler_vm path]"
+		echo "${RED}Error - wrong path: $tmp"
+		echo "${YELLOW}List of maps and players to test you can always change in the head of the ./filler_checker.zsh file"
+		echo "${BLUE}Usage: ./filler_checker.zsh [xlogin.filler] [filler_vm_path]"
+		echo "Example: ./filler_checker.zsh ../filler/vbrazas.filler resources/"
 		exit 1;
 	fi
 }
@@ -36,40 +38,41 @@ fill_arg()
 		tmp=$2
 	fi
 
-	check_arg "ls $tmp" 0 $0
+	check_arg "ls $tmp" 0
 
 	eval "$4=$tmp"
 }
 fill_arg $0 "$1" "xlogin.filler" FILE_TO_CHECK
 fill_arg $0 "$2" "./filler_vm" VM_PATH
 
-check_arg "ls $VM_PATH/players/" 0 $0
+check_arg "ls $VM_PATH/players/" 0
 PLAYERS_D="$VM_PATH/players/"
-check_arg "ls $VM_PATH/players/" 0 $0
+check_arg "ls $VM_PATH/players/" 0
 MAPS_D="$VM_PATH/maps/"
 
 
 sychronize()
 {
-	for (( l = $(pgrep ruby | wc -l); l > $1; l = $(pgrep ruby | wc -l) )); do
-		echo "${YELLOW}Waiting... for $l proccesses"
+	for (( l = $(pgrep ruby | wc -l); l > 0; l = $(pgrep ruby | wc -l) )); do
+		echo "Waiting... for $l proccesses"
 		sleep 5
 	done
 }
 
 
-check_arg "./$VM_PATH/filler_vm -q -f ${MAPS_D}$(echo $MAPS | cut -d' ' -f1) -p1 ./$FILE_TO_CHECK -p2 ./${PLAYERS_D}$(echo $PLAYERS | cut -d' ' -f1).filler" 1 $0
+check_arg "./$VM_PATH/filler_vm -q -f ${MAPS_D}$(echo $MAPS | cut -d' ' -f1) -p1 ./$FILE_TO_CHECK -p2 ./${PLAYERS_D}$(echo $PLAYERS | cut -d' ' -f1).filler" 1
 echo -n > $TEMPFILE
+echo -n $BLUE
 for	pname in `echo $PLAYERS`; do
 	for mapname in `echo $MAPS`; do
 		for (( i = 0; i < $NUM_TESTS; i++ )); do
-			./$VM_PATH/filler_vm -q -f ${MAPS_D}$mapname -p2 ./$FILE_TO_CHECK -p1 ./${PLAYERS_D}${pname}.filler | grep -B 2 "==" >> $TEMPFILE &
-			./$VM_PATH/filler_vm -q -f ${MAPS_D}$mapname -p1 ./$FILE_TO_CHECK -p2 ./${PLAYERS_D}${pname}.filler | grep -B 2 "==" >> $TEMPFILE &
-			sychronize $MAX_NUM_OF_FILLER_VM_PRCS
+			(./$VM_PATH/filler_vm -q -f ${MAPS_D}$mapname -p2 ./$FILE_TO_CHECK -p1 ./${PLAYERS_D}${pname}.filler | grep -B 2 "==" >> $TEMPFILE; echo $mapname >> $TEMPFILE) &
+			(./$VM_PATH/filler_vm -q -f ${MAPS_D}$mapname -p1 ./$FILE_TO_CHECK -p2 ./${PLAYERS_D}${pname}.filler | grep -B 2 "==" >> $TEMPFILE; echo $mapname >> $TEMPFILE) &
+			if (( $(pgrep ruby | wc -l) >= $MAX_NUM_OF_FILLER_VM_PROCESSES )); then sychronize; fi
 		done
 	done
 done
-sychronize 0
+sychronize
 
 correct=0
 all=0
@@ -83,20 +86,23 @@ while read -r line; do
 		score1=$(echo $line | cut -d' ' -f4 )
 	elif (( i==4 )); then
 		score2=$(echo $line | cut -d' ' -f4 )
+	elif (( i==5 )); then
+		map=$line
 		i=0
 	fi
 
 	if (( i==0 )); then
 		if [[ $(echo $p1 | grep "$FILE_TO_CHECK") ]]; then
-			if (( $score1 >= $score2 )); then color=$GREEN; let correct++; else color=$RED; fi
+			(( $score1 >= $score2 )) && { color=$GREEN; let correct++; } || { color=$RED; }
 		elif [[ $(echo $p2 | grep "$FILE_TO_CHECK") ]]; then
-			if (( $score2 >= $score1 )); then color=$GREEN; let correct++; else color=$RED; fi
+			(( $score2 >= $score1 )) && { color=$GREEN; let correct++; } || { color=$RED; }
 		fi
 		let all++
-		echo "${color}$p1 VS $p2 => $score1 VS $score2${RESET}"
+		echo "${color}ON MAP $map FOUGHT $p1 VS $p2 => $score1 VS $score2${RESET}"
 	fi
 	let i++;
 
 done < "$TEMPFILE"
 
 echo "${BLUE}Your result: $correct/$all"
+rm -f filler.trace
